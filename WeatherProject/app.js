19909765f5b8606df9da8e67bce515c5943b91d4
Erick Vivas <88099754+erickvivas415@users.noreply.github.com>
2022-05-11 20:15:28 -0700

    API WEATHER MAP San francisco with post request

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const https = require("https");
 const { hasUncaughtExceptionCaptureCallback } = require("process");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=san+francisco&appid=1707f794027617a2d2a4dc1d53bd972f&units=imperial"
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res) {
+    console.log(req.body.cityName);
+    console.log("POST request received.");
+    const query = req.body.cityName;
+    const apiKey = "1707f794027617a2d2a4dc1d53bd972f";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=imperial";
     https.get(url, function(response) {
         console.log(response.statusCode);
 
@@ -16,7 +27,7 @@ app.get("/", function(req, res) {
             const imageURL = "http://openweathermap.org/img/wn/" + icon +"@2x.png";
             console.log(weatherData.main.feels_like);
             res.write("<h1>The weather is currently " + weatherData.weather[0].description + "</h1>");
-            res.write("<p>The temperature in San Francisco is " + temp + " degrees</p>");
+            res.write("<p>The temperature in "+ query + " is " + temp + " degrees</p>");
             res.write("<img src=" + imageURL +" >");
             res.send();
             //res.send("The temperature in San Francisco is " + temp + " degrees");
@@ -24,8 +35,12 @@ app.get("/", function(req, res) {
         })
 
     })
+});
+
+/*
+
     //res.send("Server is up and running");
-})
+*/
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
