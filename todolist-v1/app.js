9bacd37b3caecda34e2674605071934347b0119e
Erick Vivas <88099754+erickvivas415@users.noreply.github.com>
2022-05-15 16:28:19 -0700

    input and templates on nodeJs with EJS

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -6,43 +6,41 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+// var item = ""; only pass one item
+var items = []; // An array allow us to pass multiple items
+
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get('/', (req, res) => {
-    var today = new Date();
-    var currentDate = today.getDay();
-    var day = "";
+    var today  = new Date();
+    var options = { 
+        weekday: 'long', 
+        //year: 'numeric', 
+        month: 'long', 
+        day: 'numeric' 
+    };
+    //var today  = new Date();
 
-    switch(currentDate) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-        default:
-            console.log("Error, day is not assign to any case")
-    }
-        
-    res.render("list", {kindOfday: day} );
+console.log(today.toLocaleDateString("en-US")); // 9/17/2016
+console.log(today.toLocaleDateString("en-US", options));
+
+    //var currentDate = today.getDay();
+    var day = today.toLocaleDateString("en-US", options);
+
+    console.log(items);
+    res.render("list", {kindOfday: day, newListItems: items} );
         //res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/', function(req, res) {
+    var item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
 });
 
 
