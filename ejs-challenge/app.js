2ca0762ce25e6d733589f21eaad9e15c4b73094e
Erick Vivas <88099754+erickvivas415@users.noreply.github.com>
2022-05-19 19:33:04 -0700

    blog with compose

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https");
 const ejs = require("ejs");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -11,6 +12,8 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.render("home", {startingContent: homeStartingContent});
@@ -22,6 +25,18 @@ app.get('/about', (req, res) => {
 
 app.get('/contact', (req, res) => {
   res.render("contact", {contactContent: contactContent});
+});
+
+app.get('/compose', (req, res) => {
+  res.render("compose");
+});
+
+app.post('/compose', function(req, res) {
+  console.log(req.body.newArticle);
+  //let postArticle = [];
+  //postArticle.push(req.body.newArticle);
+  
+  //res.redirect("/");
 });
 
 
